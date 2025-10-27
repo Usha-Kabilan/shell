dfac5b87614113767c0aba0ba97f36f737760573
Usha Kabilan <usha.kabilan@utoronto.ca>
2025-10-27 14:34:46 -0400

    making sure inventory.txt goes in the right folder.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -59,6 +59,6 @@ rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
-find ./data/processed -type f > ./inventory.txt
+find ./data/processed -type f > ./data/inventory.txt
 ###########################################
 echo "Project setup is complete!"
